14bde07672770d5ced4edf7c8f9e8e7e42580c93
AnjaliiV <anjali141005@gmail.com>
2026-01-29 15:47:50 +0530

    Add files via upload

---
+++ Ring_Road.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int u,v;
+    cin>>u>>v;
+    int n;
+    cin>>n;
+
+    //clock wise:
+    for (int i = 0; i < n ; i++)
+    {
+        /* code */
+    }
+    
+
+}
